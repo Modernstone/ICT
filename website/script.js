003a427e40406883f3c8aef3913e9538a97ecd91
Modernstone <vishupatel9766@gmail.com>
2024-10-29 19:05:58 +0530

    u3
    update 3

--- website/script.js
+++
@@ -1,7 +0,0 @@
-function showSemester(semesterId) {
-    var semesters = document.getElementsByClassName("semester");
-    for (var i = 0; i < semesters.length; i++) {
-        semesters[i].style.display = "none";
-    }
-    document.getElementById(semesterId).style.display = "block";
-}
